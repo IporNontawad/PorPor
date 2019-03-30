b385fc7b2d32c44a0420ac9a9f070be400c80fc4
ImangPor <ipornontawad@gmail.com>
2019-03-30 13:34:57 +0700

    workshop1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { getOrCreateInjectable } from '@angular/core/src/render3/di';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-dream-app';
+
+  name:string ="Mac";
+  score:string ="44";
+
+  student = {
+    name:"Kwang",
+    studentID : "5921602795",
+    weight : 51,
+    height : 171
+  }
+  studentlist = [{
+    name:"M1",
+    studentID : "5921602795",
+    weight : 51,
+    height : 171
+  },
+  {
+    name:"M2",
+    studentID : "5921602795",
+    weight : 51,
+    height : 171
+  },
+  {
+    name:"M3",
+    studentID : "5921602795",
+    weight : 51,
+    height : 171
+  }
+
+
+
+
+
+]
+  constructor(){
+  this.studentlist.map((object,index)=>{
+  let obj:any= object;
+  obj.bmi = object.weight/((object.height/100)*(object.height/100))
+  return obj;
+ 
+  
+    let student ="student in function";
+    console.log(this.studentlist);
+    console.log(student);
+  })}
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
